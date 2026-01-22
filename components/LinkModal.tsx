4d507b42a14199e62c61b052c303e9e55195cb12
ltuu <8ja9M5D1g0S4hfFbN4YDkB+ltuu@noreply.cnb.cool>
2026-01-22 10:52:37 +0800

    更换链接图标获取API为favicon.org.cn
    - LinkModal.tsx: 使用新API自动获取链接图标
    - SearchSettingsModal.tsx: 搜索引擎图标获取改用新API
    - SettingsModal.tsx: 添加链接时图标获取改用新API
    - 新API: https://favicon.org.cn/get.php\?url\=URL\&size\=128\&key\=...

--- components/LinkModal.tsx
+++ components/LinkModal.tsx
@@ -62,9 +62,8 @@ const LinkModal: React.FC<LinkModalProps> = ({ isOpen, onClose, onSave, categori
             normalizedUrl = 'https://' + targetUrl;
         }
 
-        // Use favicon.ico directly
-        const urlObj = new URL(normalizedUrl);
-        const newIcon = `${urlObj.origin}/favicon.ico`;
+        // Use favicon.org.cn API
+        const newIcon = `https://favicon.org.cn/get.php?url=${encodeURIComponent(normalizedUrl)}&size=128&key=usr-09b4268ccbf0b297611dc1a02fde7f739eec7ac3`;
 
         setIconUrl(newIcon);
       } catch (e) {
