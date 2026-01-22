612e7d7ad0667d5deb8e7e9864c2570a975d69e0
ltuu <8ja9M5D1g0S4hfFbN4YDkB+ltuu@noreply.cnb.cool>
2026-01-22 11:03:17 +0800

    修复：SettingsModal中的favicon API语法错误

--- components/SettingsModal.tsx
+++ components/SettingsModal.tsx
@@ -399,8 +399,7 @@ async function saveLink(title, url, categoryId, icon = '') {
 
     if (!icon) {
         try {
-            const u = new URL(url);
-            icon = `https://favicon.org.cn/get.php?url=${encodeURIComponent(url)}&size=128&key=usr-09b4268ccbf0b297611dc1a02fde7f739eec7ac3`;
+            icon = "https://favicon.org.cn/get.php?url=" + encodeURIComponent(url) + "&size=128&key=usr-09b4268ccbf0b297611dc1a02fde7f739eec7ac3";
         } catch(e){}
     }
 
